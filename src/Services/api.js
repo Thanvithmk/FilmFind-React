a07ceb422d5874e8fa594911dc6518f157a8a6b5
Thanvithmk <thanvithmk@gmail.com>
2025-08-14 10:58:30 +0530

    .env file added

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,10 +1,10 @@
-//const API_KEY ="f05484e5540d0188a4b5a89f4d81873e"
-const API_KEY = "f05484e5540d0188a4b5a89f4d81873e";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3"; 
+
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    if (!response.ok) { // This block should be inside the function
+    if (!response.ok) {
         throw new Error("Network response was not ok");
     }
     const data = await response.json();
@@ -13,7 +13,7 @@ export const getPopularMovies = async () => {
 
 export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    if (!response.ok) { // This block should be inside the function
+    if (!response.ok) {
         throw new Error("Network response was not ok");
     }
     const data = await response.json();
